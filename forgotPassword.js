d4028097e9f265357a11845b5e9bb8967c84dd7b
roy845 <58545785+roy845@users.noreply.github.com>
2021-01-13 14:41:58 +0200

    sdgs
    sdfsdf

--- forgotPassword.js
+++ forgotPassword.js
@@ -3,6 +3,10 @@ function ForgotPasswordValFunction(){
     
     var emailField = document.getElementById("exampleInputEmail");
     var email = document.getElementById("exampleInputEmail").value;
+    var resetBtn = document.getElementById("signInBtn");
+    var flagRecaptcha = checkRecaptcha();
+    var flag=true;
+
     //email validation
     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(!email.match(mailformat))
@@ -11,5 +15,14 @@ function ForgotPasswordValFunction(){
         emailField.title="you entered invalid email\nformat need to be:\n[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]@[a-zA-Z0-9-].[a-zA-Z0-9-]";
         return false;
     }
-    return true;
+
+    if(flag==true&&flagRecaptcha==true) {
+        resetBtn.disabled = false;
+        return true;
+       }
+        else{
+            resetBtn.disabled = true;
+        return false;
+        }
+    
 }
