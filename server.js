336c854c37196b6975ddf1c596f8c7b12ccd9e97
roy845 <58545785+roy845@users.noreply.github.com>
2021-01-13 21:42:56 +0200

    Update server.js
    luglkguku

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get("/usernotfoundforgot",function(req,res){
 
     //======================== GET PASSWORD-UPDATE PAGE ========================//
     app.get("/updatepassword",function(req,res){
-      dec= decryptData(req.query.userID);
+      dec= decryptData(req.query);
       console.log("id is",dec);
       res.cookie("Forget",dec['id'],{maxAge:1*60*60*1000,httpOnly:true});
       res.sendFile(__dirname+"/update-password.html",);
@@ -318,7 +318,7 @@ app.post("/forgotPass",function(req,res){
                     var userId=result.rows[0].id;
                     var obj={id:userId};
                     enc=encryptData(obj);
-                    var refere='https://tomro95-heroku-app.herokuapp.com/updatepassword?userID='+enc;
+                    var refere='https://tomro95-heroku-app.herokuapp.com/updatepassword?'+enc;
                     var mailOptions = {
                       from: 'wefixbraudeproject@gmail.com',
                       to: email,
