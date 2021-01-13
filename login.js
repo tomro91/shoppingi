325e3d62b95ada6556277fcbe82b28d5c6772cb5
tomro91 <57315297+tomro91@users.noreply.github.com>
2021-01-13 14:00:12 +0200

    recapthcha

--- login.js
+++ login.js
@@ -1,18 +1,5 @@
 
-  function checkRecaptcha() 
-  {
-      var v = grecaptcha.getResponse();
-      if(v.length == 0)
-      {
-          document.getElementById('captcha').innerHTML="You can't leave Captcha Code empty";
-          return false;
-      }
-      else
-      {
-           document.getElementById('captcha').innerHTML="Captcha completed";
-          return true; 
-      }
-  }
+ 
 
 
 function signInValidate() 
