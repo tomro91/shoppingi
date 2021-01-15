bce059b734c71d459cd83ce86d8a89a02f870ddf
tomro91 <57315297+tomro91@users.noreply.github.com>
2021-01-15 18:10:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -367,7 +367,7 @@ app.post("/forgotPass",function(req,res){
 
 
 //listening to port 5500
-app.listen(process.env.PORT||5500,host);
+app.listen(process.env.PORT||3000);
 console.log("listening....");
 client.connect()
 .then(() => console.log("client Connected to database successfuly"))
