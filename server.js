6b6a2cd4481d0e60c3952cdf0e339710fe29b198
tomro91 <57315297+tomro91@users.noreply.github.com>
2021-01-15 17:59:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,38 +24,7 @@ const client = new Client({
   database: "d2831bevpqn6sp"
 })
 
-//======================FUNCTIONS====================================
-function encryptData(data) {
-    try {
-    let iv = crypto.randomBytes(16);
-    let key = crypto.pbkdf2Sync(secret, salt, rounds, keySize, 'sha512');
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-    let encryptedData = Buffer.concat([cipher.update(JSON.stringify(data)), cipher.final()]);
-    return iv.toString('base64') + ':' + encryptedData.toString('base64');
-    }
-    catch (err) {
-    console.error(err)
-    return false;
-    }
-  }
 
-  function decryptData(encData) {
-    try {
-    let textParts = encData.split(':');
-    let iv = Buffer.from(textParts.shift(), 'base64');
-    let encryptedData = Buffer.from(textParts.join(':'), 'base64');
-    let key = crypto.pbkdf2Sync(secret, salt, rounds, keySize, 'sha512');
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-    let decryptedData = decipher.update(encryptedData);
-    decryptedData = Buffer.concat([decryptedData, decipher.final()]);
-    return JSON.parse(decryptedData.toString());
-    }
-    catch (err) {
-    console.error(err)
-    return false;
-    }
-    }
-    
 
 
 //======================== GET REQUESTS SECTION ========================//
