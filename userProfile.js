5b095f60e0ff5eaaec7de59afb2e4f3e727c5436
tomro91 <57315297+tomro91@users.noreply.github.com>
2021-01-15 18:56:23 +0200

    logout fix

--- userProfile.js
+++ userProfile.js
@@ -1,6 +1,5 @@
 function userProfileValidate()
 {
-
 var oldPasswordField= document.getElementById("inputOldPassword");
 var oldPassword= document.getElementById("inputOldPassword").value;
 var newPasswordField= document.getElementById("inputNewPassword");
@@ -8,11 +7,11 @@ var confirmPasswordField= document.getElementById("inputConfirmPassword");
 var newPassword= document.getElementById("inputNewPassword").value;
 var confirmPassword= document.getElementById("inputConfirmPassword").value;
 
-
 if(oldPassword=="")
 {
     oldPasswordField.focus();
-    oldPasswordField.title="old password empty!!"  
+    oldPasswordField.title="old password empty!!";
+    
     return false;
 }
 
@@ -102,30 +101,3 @@ msgPassword="Invalid password:\n";
 
 
 
-/*// When the user clicks on any of the password fields, show the message box
-oldPasswordField.onfocus = function() {
-    document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-oldPasswordField.onblur = function() {
-    document.getElementById("message").style.display = "none";
-}
-
-newPasswordField.onfocus = function() {
-    document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-newPasswordField.onblur = function() {
-    document.getElementById("message").style.display = "none";
-}
-
-confirmPasswordField.onfocus = function() {
-    document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-confirmPasswordField.onblur = function() {
-    document.getElementById("message").style.display = "none";
-}*/
