00ac313c4acba8597b9f47e874632525f4e2f0a5
roy845 <58545785+roy845@users.noreply.github.com>
2021-01-15 15:05:09 +0200

    sdsdf

--- userProfile.js
+++ userProfile.js
@@ -1,7 +1,106 @@
+function userProfileValidate()
+{
+
 var oldPasswordField= document.getElementById("inputOldPassword");
+var oldPassword= document.getElementById("inputOldPassword").value;
 var newPasswordField= document.getElementById("inputNewPassword");
 var confirmPasswordField= document.getElementById("inputConfirmPassword");
-// When the user clicks on any of the password fields, show the message box
+var newPassword= document.getElementById("inputNewPassword").value;
+var confirmPassword= document.getElementById("inputConfirmPassword").value;
+
+
+if(oldPassword=="")
+{
+    oldPasswordField.focus();
+    oldPasswordField.title="old password empty!!"  
+    return false;
+}
+
+if(newPassword=="")
+{
+    newPasswordField.focus();
+    newPasswordField.title="new password empty!!"  
+    return false;
+}
+
+if(confirmPassword=="")
+{
+    confirmPasswordField.focus();
+    confirmPasswordField.title="confirm password empty!!"  
+    return false;
+}
+
+
+msgPassword="Invalid password:\n";
+        var flag=true;
+        if(newPassword.length<6)
+        {
+            msgPassword+="minimum of 6 letters\n"; 
+            flag=false;
+        }
+         
+        if(!newPassword.match(/[a-z]/g)){
+            msgPassword+="Must contains lowercase letter\n"; 
+            flag=false;
+          
+        }
+        
+        if(!newPassword.match(/[A-Z]/g)){
+          msgPassword+="Must contains uppercase letter\n"; 
+          flag=false;
+        }
+          if(!newPassword.match(/[0-9]/g))  {
+          msgPassword+="Must contains at least one digit\n";
+          flag=false;
+          }
+          if(!newPassword.match( /[^a-zA-Z\d]/g)){
+          msgPassword+="Must contains at least one special key\n";
+          flag=false;
+          }
+          if(!newPassword.match( /^[a-zA-Z0-9!@#\$%\^\&*\)\({\}+=.;:_-]+$/g)){
+                msgPassword+="contains unsupported keys\nsupported keys are:! @ # $ % ^ & * ( ) - _ = + \ | [ ] { } ; : / ? . > <";
+                flag=false;
+                }
+           if(flag==false){
+            newPasswordField.focus();
+            newPasswordField.title=msgPassword;
+            
+            return false;
+              
+           }
+           
+           if(newPassword!=confirmPassword){
+                
+            newPasswordField.title="passwords are not matched";
+            newPasswordField.focus();
+                confirmPasswordField.title="passwords are not matched";
+               return false;
+           }
+           return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// When the user clicks on any of the password fields, show the message box
 oldPasswordField.onfocus = function() {
     document.getElementById("message").style.display = "block";
 }
@@ -27,4 +126,4 @@ confirmPasswordField.onfocus = function() {
 // When the user clicks outside of the password field, hide the message box
 confirmPasswordField.onblur = function() {
     document.getElementById("message").style.display = "none";
-}
+}*/
